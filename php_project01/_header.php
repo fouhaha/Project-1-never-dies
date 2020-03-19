d9d33aeca673523c2e6f754708e3cd584dbcf1be
fouhaha <francois.frk@gmail.com.com>
2020-03-19 13:21:22 +0100

    commit with include(_header.php)

---
+++ php_project01/_header.php
@@ -0,0 +1,78 @@
+<?php
+
+switch($pageTitle) {
+    case 'howto':
+        $currentPageTitle = 'How to learn to code - Formations';
+        break;
+    case 'index':
+        $currentPageTitle = 'How to learn to code - Accueil';
+        break;
+    case 'infos':
+        $currentPageTitle = 'How to learn to code - Infos';
+        break;
+    case 'ressources':
+        $currentPageTitle = 'How to learn to code - Ressources';
+        break;
+    case 'roadmap':
+        $currentPageTitle = 'How to learn to code - Front ou Back ?';
+        break;
+}
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada|Roboto|Roboto+Condensed&display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/3fa526640b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style/style.css">
+    <title><?php echo $currentPageTitle; ?></title> <!-- A CHANGER SELON LE NOM DE VOTRE PAGE -->
+</head>
+
+<body>
+<header>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="index.php"><i class="fab fa-connectdevelop fa-2x"></i>How To Learn To Code</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <!-- RAJOUTER LA CLASSE ACTIVE SUR LE li du LIEN CORRESPONDAND A VOTRE PAGE -->
+                    <a class="nav-link active" href="index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="roadmap.php">Front ou Back ?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="howto.php">Formations</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ressources.php">Ressources</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="infos.php">Infos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
